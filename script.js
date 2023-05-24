a584855a6890427db4dc62e8bb65e35ac0169784
sh-islam <misla2@uwo.ca>
2023-05-23 21:46:31 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,80 @@
+//  wait for the DOM to be fully loaded before executing
+// allows buttons to be avaialbe in DOM 
+document.addEventListener("DOMContentLoaded", function() {
+    const buttons = document.querySelectorAll("button");
+
+    function handleKeyPress(event){
+      if (event.shiftKey && event.key === 'F1'){
+        button1 = document.getElementById("button1");
+        soundFile = `/sounds/${button1.name}.wav`
+        if (button1.getAttribute('playing') === 'false'){playSound(soundFile, button1)}
+      }
+    }
+    document.addEventListener("keydown", handleKeyPress);
+
+    function playSound(soundFile, button) {
+      if (soundFile) {
+        const audio = new Audio(soundFile);
+        audio.play();
+        button.setAttribute('playing', true);
+        // audio.addEventListener("ended", function(){
+        //   button.setAttribute('playing', false);
+        // })
+        setTimeout(function() {
+          audio.pause();
+          audio.currentTime = 0;
+          button.setAttribute('playing', false);
+        }, 2500);
+      }
+    }
+    
+    buttons.forEach(function(button) {
+      button.addEventListener("click", function(){
+        let soundFile = `/sounds/${button.name}.wav`;
+        if (button.getAttribute('name') === 'PartyHorn'){
+          soundFile = `/sounds/${button.name}.mpeg`;
+        }
+        if (button.getAttribute('playing') === 'false'){playSound(soundFile, button)}
+       
+      })
+        // button.addEventListener("click", function() {
+        //   switch (button.id) {
+        //     case "button1":
+        //       console.log("Button 1 clicked");
+        //       break;
+
+        //     case "button2":
+        //       console.log("Button 2 clicked");
+        //       break;
+
+        //     case "button3":
+        //       console.log("Button 3 clicked");
+        //       break;
+
+        //     case "button4":
+        //     console.log("Button 4 clicked");
+        //     break;
+
+        //     case "button5":
+        //     console.log("Button 5 clicked");
+        //     break;
+
+        //     case "button6":
+        //     console.log("Button 6 clicked");
+        //     break;
+
+        //     default:
+        //       break;
+        //   }
+        // });
+        
+      });
+    // function greet(event) {
+    //   console.log("greet:", event);
+    // }
+  
+    // buttons.forEach(function(button) {
+    //   button.addEventListener("click", greet);
+    // });
+  });
+  
